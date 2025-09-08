4cf285bac1da5800194501cf0b21e6b1ef50c8fc
Susan Gakii <susangakii71@gmail.com>
2025-09-08 11:32:04 +0300

    array filter

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+function findMatching(drivers, string){
+    return drivers.filter (driver => driver.toLowerCase() === string.toLowerCase());
+}
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter (driver => driver.toLowerCase().startsWith(string.toLowerCase()));
+}
+
+function matchName(drivers, string){
+    return drivers.filter (driver => driver.name.toLowerCase() === string.toLowerCase());
+}
